c9be8d25fb22953b6184f9adbeb092ff4ffe955f
gustavodileone <56900764+gustavodileone@users.noreply.github.com>
2024-11-17 15:07:15 -0300

    rewritten

--- main.c
+++ main.c
@@ -1,184 +1,166 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <time.h>
 
 #include <X11/Xlib.h>
-#include <X11/Xutil.h>
 
-typedef struct Program {
+#define DEFAULT_WIDTH 500
+#define DEFAULT_HEIGHT 500
+#define FRAMES_PER_SECOND 90
+
+typedef struct App {
     Display* dp;
     Window w;
     GC gc;
     Pixmap pm;
-    int screen, x, y, width, height;
-} Program;
+    int screen, width, height;
+} App;
 
-typedef struct DoomFire {
+typedef struct Doom {
     unsigned long palette[36];
-    unsigned long *area;
-    int area_size;
-} DoomFire;
+    short *frame;
+    size_t frame_length;
+} Doom;
 
-unsigned long RGB(int r, int g, int b) {
+unsigned long rgb(int r, int g, int b) {
     return b + (g << 8) + (r << 16);
 }
 
-DoomFire init_doom_fire(int width, int height) {
-    DoomFire f;
-    f.palette[0] = RGB(0, 0, 0);
-    f.palette[1] = RGB(31, 7, 7);
-    f.palette[2] = RGB(47, 15, 7);
-    f.palette[3] = RGB(71, 15, 7);
-    f.palette[4] = RGB(87, 23, 7);
-    f.palette[5] = RGB(103, 31, 7);
-    f.palette[6] = RGB(119, 31, 7);
-    f.palette[7] = RGB(143, 39, 7);
-    f.palette[8] = RGB(159, 47, 7);
-    f.palette[9] = RGB(175, 63, 7);
-    f.palette[10] = RGB(191, 71, 7);
-    f.palette[11] = RGB(199, 71, 7);
-    f.palette[12] = RGB(223, 79, 7);
-    f.palette[13] = RGB(223, 87, 7);
-    f.palette[14] = RGB(223, 87, 7);
-    f.palette[15] = RGB(215, 95, 7);
-    f.palette[16] = RGB(215, 103, 15);
-    f.palette[17] = RGB(207, 111, 15);
-    f.palette[18] = RGB(207, 119, 15);
-    f.palette[19] = RGB(207, 127, 15);
-    f.palette[20] = RGB(207, 135, 23);
-    f.palette[21] = RGB(199, 135, 23);
-    f.palette[22] = RGB(199, 143, 23);
-    f.palette[23] = RGB(199, 151, 31);
-    f.palette[24] = RGB(191, 159, 31);
-    f.palette[25] = RGB(191, 159, 31);
-    f.palette[26] = RGB(191, 167, 39);
-    f.palette[27] = RGB(191, 167, 39);
-    f.palette[28] = RGB(191, 175, 47);
-    f.palette[29] = RGB(183, 175, 47);
-    f.palette[30] = RGB(183, 183, 47);
-    f.palette[31] = RGB(183, 183, 55);
-    f.palette[32] = RGB(207, 207, 111);
-    f.palette[33] = RGB(223, 223, 159);
-    f.palette[34] = RGB(239, 239, 199);
-    f.palette[35] = RGB(255, 255, 255);
+void sleep_by_fps(int fps) {
+    struct timespec time;
+    time.tv_sec = 0;
+    time.tv_nsec = 1000000000 / fps;
 
-    f.area_size = width * height;
-    f.area = malloc(sizeof(unsigned long) * f.area_size);
-    for(int i = 0; i < f.area_size; i++) {
-        f.area[i] = i >= (f.area_size - width) ? 35 : 0;
-    }
-
-    return f;
+    nanosleep(&time, NULL);
 }
 
-void update_doom_fire(DoomFire* f, int width, int height) {
-    free(f->area);
-
-    f->area_size = width * height;
-    f->area = malloc(f->area_size * sizeof(unsigned long));
-    for(int i = 0; i < f->area_size; i++) {
-        f->area[i] = i >= (f->area_size - width) ? 35 : 0;
+void fill_doom(Doom* doom, int width) {
+    int last_index = (sizeof(doom->palette) / sizeof(doom->palette[0])) - 1;
+    for(int i = 0; i < doom->frame_length; i++) {
+        doom->frame[i] = i >= (doom->frame_length - width) ? last_index : 0;
     }
 }
 
-void render_doom_fire(DoomFire* f, Program* p) {
-    for(int y = 0; y < p->height; y++) {
-        for(int x = 0; x < p->width; x++) {
-            int current_index = x + (p->width * y);
-            int bellow_index = current_index + p->width;
-            if(bellow_index >= f->area_size) continue;
+Doom init_doom(App app) {
+    Doom doom = {
+        palette: { rgb(7, 7, 7), rgb(31, 7, 7), rgb(47, 15, 7), rgb(71, 15, 7), rgb(87, 23, 7), rgb(103, 31, 7), rgb(119, 31, 7), rgb(143, 39, 7), rgb(159, 47, 7), rgb(175, 63, 7), rgb(191, 71, 7), rgb(199, 71, 7), rgb(223, 79, 7), rgb(223, 87, 7), rgb(223, 87, 7), rgb(215, 95, 7), rgb(215, 103, 15), rgb(207, 111, 15), rgb(207, 119, 15), rgb(207, 127, 15), rgb(207, 135, 23), rgb(199, 135, 23), rgb(199, 143, 23), rgb(199, 151, 31), rgb(191, 159, 31), rgb(191, 159, 31), rgb(191, 167, 39), rgb(191, 167, 39), rgb(191, 175, 47), rgb(183, 175, 47), rgb(183, 183, 47), rgb(183, 183, 55), rgb(207, 207, 111), rgb(223, 223, 159), rgb(239, 239, 199), rgb(255, 255, 255) },
+        frame_length: app.width * app.height,
+        frame: NULL,
+    };
 
-            int decay = f->area[bellow_index] - (rand() % 2);
-            f->area[current_index] = decay >= 0 ? decay : 0;
+    doom.frame = malloc(doom.frame_length * sizeof(doom.frame[0]));
+    if(doom.frame == NULL) {
+        fprintf(stderr, "Couldn't initialize memory for the frame.\n");
+        exit(EXIT_FAILURE);
+    }
 
-            XSetForeground(p->dp, p->gc, f->palette[f->area[current_index]]);
-            XDrawPoint(p->dp, p->pm, p->gc, x, y);
+    fill_doom(&doom, app.width);
+
+    return doom;
+}
+
+void render_doom(App app, Doom doom) {
+    int four_halves = app.width / 4;
+
+    for(int y = 0; y < app.height; y++) {
+        for(int x = 0; x < four_halves; x++) {
+            int i = x + (y * app.width);
+
+            int bellow = i + app.width;
+            if(bellow >= doom.frame_length) continue;
+
+            int decayed = doom.frame[bellow] - (rand() % 2);
+            doom.frame[i] = decayed > 0 ? decayed : 0;
+
+            XSetForeground(app.dp, app.gc, doom.palette[doom.frame[i]]);
+            XDrawPoint(app.dp, app.pm, app.gc, x, y);
         }
     }
 
-    XCopyArea(p->dp, p->pm, p->w, p->gc, 0, 0, p->width, p->height, 0, 0);
-    XSync(p->dp, 0);
+    for(int i = 0; i < app.width; i = i + four_halves) {
+        XCopyArea(app.dp, app.pm, app.pm, app.gc, 0, 0, app.width, app.height, i, 0);
+    }
+
+    XCopyArea(app.dp, app.pm, app.w, app.gc, 0, 0, app.width, app.height, 0, 0);
 }
 
-void update_program_attr(Program* p) {
-    XWindowAttributes attr;
-    XGetWindowAttributes(p->dp, p->w, &attr);
+void resize_doom(Doom* doom, int width, int height) {
+    int length = width * height;
 
-    p->width = attr.width;
-    p->height = attr.height;
+    doom->frame = realloc(doom->frame, length * sizeof(doom->frame[0]));
+    if(doom->frame == NULL) {
+        fprintf(stderr, "Couldn't allocate memory for the frame.");
+        exit(EXIT_FAILURE);
+    }
+
+    doom->frame_length = length;
+
+    fill_doom(doom, width);
 }
 
-Program init_program() {
-    Program p;
-    p.dp = XOpenDisplay(NULL);
-    assert(p.dp);
+void resize_app(App* app, int width, int height) {
+    app->width = width;
+    app->height = height;
 
-    p.screen = XDefaultScreen(p.dp);
-
-    p.x = 0, p.y = 0;
-    p.width = 800, p.height = 320;
-
-    p.w = XCreateSimpleWindow(p.dp, XDefaultRootWindow(p.dp), p.x, p.y, p.width, p.height, 0, BlackPixel(p.dp, p.screen), BlackPixel(p.dp, p.screen));
-    XSetStandardProperties(p.dp, p.w, "Fogo DOOM", NULL, None, NULL, 0, NULL);
-    XSelectInput(p.dp, p.w, StructureNotifyMask | KeyPressMask);
-    XMapWindow(p.dp, p.w);
-
-    p.gc = XCreateGC(p.dp, p.w, 0, NULL);
-
-    return p;
+    XFreePixmap(app->dp, app->pm);
+    app->pm = XCreatePixmap(app->dp, app->w, width, height, DefaultDepth(app->dp, app->screen));
 }
 
-void close_program(Program p) {
-    XFreePixmap(p.dp, p.pm);
-    XFreeGC(p.dp, p.gc);
-    XDestroyWindow(p.dp, p.w);
-    XCloseDisplay(p.dp);
+App init_app() {
+    App app = {0};
+    app.width = DEFAULT_WIDTH;
+    app.height = DEFAULT_HEIGHT;
 
-    exit(EXIT_SUCCESS);
+    Display* dp = XOpenDisplay(NULL);
+    
+    app.dp = dp;
+    app.screen = DefaultScreen(dp);
+
+    app.w = XCreateSimpleWindow(dp, DefaultRootWindow(dp), 0, 0, app.width, app.height, 0, 0, BlackPixel(dp, app.screen));
+    app.gc = XCreateGC(dp, app.w, 0, NULL);
+    app.pm = XCreatePixmap(dp, app.w, app.width, app.height, DefaultDepth(dp, app.screen));
+
+    XSelectInput(dp, app.w, StructureNotifyMask);
+    XMapWindow(dp, app.w);
+
+    return app;
+}
+
+void destroy_app(App app) {
+    XCloseDisplay(app.dp);
 }
 
 int main() {
     srand(time(NULL));
-    Program p = init_program();
 
-    XEvent e;
-    while(1) {
-        XNextEvent(p.dp, &e);
-        if(e.type == MapNotify) {
-            update_program_attr(&p);
-            break;
-        }
-    }
-
-    p.pm = XCreatePixmap(p.dp, p.w, p.width, p.height, DefaultDepth(p.dp, p.screen));
-
-    DoomFire f = init_doom_fire(p.width, p.height);
-
-    struct timespec t;
-    t.tv_sec = 0;
-    t.tv_nsec = 1000000000 / 60; // 60 FPS
+    App app = init_app();
+    Doom doom = init_doom(app);
 
     while(1) {
-        while(XPending(p.dp)) {
-            XNextEvent(p.dp, &e);
+        if(XPending(app.dp)) {
+            XEvent ev;
+            XNextEvent(app.dp, &ev);
 
-            if(e.type == ConfigureNotify && (e.xconfigure.width != p.width || e.xconfigure.height != p.height)) {
-                update_program_attr(&p);
-                XFreePixmap(p.dp, p.pm);
-                p.pm = XCreatePixmap(p.dp, p.w, p.width, p.height, DefaultDepth(p.dp, p.screen));
-                update_doom_fire(&f, p.width, p.height);
+            if(ev.type == MapNotify) {
+                XWindowAttributes wattr;
+                XGetWindowAttributes(app.dp, app.w, &wattr);
+                if(wattr.width == app.width && wattr.height == app.height) continue;
+                
+                resize_app(&app, wattr.width, wattr.height);
+                resize_doom(&doom, app.width, app.height);
             }
 
-            char text[1];
-            if(e.type == KeyPress && XLookupString(&e.xkey, text, sizeof(text), NULL, NULL) > 0) {
-                if(text[0] == 'q' || text[0] == 'Q') close_program(p);
+            if(ev.type == ConfigureNotify && (ev.xconfigure.width != app.width || ev.xconfigure.height != app.height)) {
+                resize_app(&app, ev.xconfigure.width, ev.xconfigure.height);
+                resize_doom(&doom, app.width, app.height);
             }
         }
 
-        render_doom_fire(&f, &p);
-        nanosleep(&t, NULL);
+        render_doom(app, doom);
+        sleep_by_fps(FRAMES_PER_SECOND);
     }
+
+    destroy_app(app);
 
     return EXIT_SUCCESS;
 }
